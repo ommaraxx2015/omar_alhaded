2da7efe90f8e825f0c085a99eff90c6f39db5b68
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2020-12-26 18:06:42 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,42 @@
+/*
+$(`<img src="sico/z1eqcqs23i10.jpg" class="fr" style="width:120%;margin-top: 0px;" >
+<a id="omar_alhaded3" href="" style="border-radius: 3px;width:23%;margin:2px;" class="fl mini  fa  fa-
+ btn btn-primary"> فيس بوك</a>
+<a id="omar_alhaded3" href="" style="border-radius: 3px;width:23%;margin:2px;" class="fl mini  fa   fa-
+ btn btn-default"> ميز نفسك</a>
+<a id="omar_alhaded3" href="" style="border-radius: 3px;width:27%;margin:2px;" class="fl mini  fa fa-
+btn btn-primary"> قوانين موقع</a>
+<a id="omar_alhaded3" href="" style="border-radius: 3px;width:22%;margin:2px;" class="fl mini  fa fa-
+btn btn-default"> اتصل بنا</a>
+
+`).insertBefore('.nav-tabs');
+*/
+$(`<div><marquee direction="right" width="100%" id="omar_alhaded1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font color="#357524" class="omar_alhaded2"> احبــك للجوال </font>افضل سوبر لهذا الاسبوع<font class="omar_alhaded2" color="#800000">  قريبـا </font>افضل زائر لهذا الاسبوع<font class="omar_alhaded2" color="#ff9900"> قريبـا </font>افضل زائرة لهذا الاسبوع<font class="omar_alhaded2" color="#ff00ff"> قريبـا </font>مبدع الحائط لهذا الاسبوع<font class="omar_alhaded2" color="#5CAD9A"> قريبـا </font>مبدعه الحائط لهذا الاسبوع<font class="omar_alhaded2" color="#800000"> قريبا  َ</font>مع تمنياتنا للجميع باطيب الاوقات&nbsp;</marquee></div>`).insertBefore('div#tlogins .lonline.light.break');
+
+$('<div class=\'fr borderg\' style=\'padding:2px;background-color:white;float:left;\'> استايلات و ارشفة :<a href=\'http://www.chatnsh.com/\'>عمر الحديد</a></div>')['insertAfter']($('.loginstat'));
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$(`${'<link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa|El+Messiri" rel="stylesheet">\x0D\x0A<link rel="stylesheet" href="https://rawcdn.githack.com/ommaraxx2015/omar_alhaded/10db1d3182ae62ad49f2b473cfb7b698b344ac7a/code.css">\x0D\x0A<style>\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    font-size: 10px!important;\x0D\x0A    padding: 0 5px!important;\x0D\x0A    position: relative;\x0D\x0A    color: #ffffff;\x0D\x0A}\x0D\x0A.fr.borderg {\x0D\x0A    border: 0px;\x0D\x0A    margin-top: 0;\x0D\x0A    height: 0;\x0D\x0A\x0D\x0A}\x0D\x0A\x0D\x0Alabel.fl.label.loginstat {\x0D\x0A    border-radius: 15px!important;\x0D\x0A    position: absolute;\x0D\x0A    top: -3px;\x0D\x0A    margin-top: 4px;\x0D\x0A    left: 7px;\x0D\x0A    padding: 1px;\x0D\x0A    height: 17px;\x0D\x0A    width: 45px;\x0D\x0A}\x0D\x0Atextarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}\x0D\x0A.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #2a6ba9;font-family: \'El Messiri\',FontAwesome;border-radius: 3px;}\x0D\x0A\x0D\x0A</style>'}`)['insertBefore']('body');
+$('.hand.nosel.fl.uzr.border')['css']({ 'margin': '0px', borderRight: '3px solid #2a6ba9', borderLeft: '3px solid #2a6ba9', borderTop: '1px solid #e2e1e1', borderBottom: '1px solid #e2e1e1', borderRadius: '3px' })
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome">لُطفاً ♥️ الإعلانات للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+$("[data-target=#wall]").attr('title',' مســآحه حره لأبداعاتكم ')
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(".fa-gear").text("الضبط").css("width","");
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$(".dpnl").css('width','310px'); 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() { alert( "اهلا وسهلا بكم في شات آحـبك  سجل معنا عضويه لتحصل على لايكات ولدايه وزخرفه وحفظ بروفايلك وكل عام وانتم بخير سنه سعيده عليكم 2021" );
+})
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<label onclick="window.open('cp?token='+token,'_blank');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-star"></span>ادارة بيانات المواقع</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');
+Tsend_=Tsend;
+reqlikes=5;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك الكتابة عام يتطلب منك الحصول على لايكات'+reqlikes+' '); return;}else{Tsend_();}}
+setprofile_=setprofile;
+reqlikes=5;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{setprofile_();}}
+$( "div#l2 button.btn.btn-primary" ).bind( "click", function() { alert( "اهلا وسهلا بكم في عائلتنا عائلة شات احبك للجوال ");
+})
+$( "div#l3 button.btn.btn-primary" ).bind( "click", function() { alert( "عند تسجيلك للعضوية فانك توافق على عدم قوانين الشات واي اختلال في القوانين سيتم طردك صاحبة الشات رانيا  المغربية وصاحب الشات عمر الحديد وزياد الشمري" );
+})
+
